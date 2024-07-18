d55ccb8ee56fd9ab718f60b44821db0d15c6efc9
speedwasswa <speedwasswa02@gmail.com>
2024-07-18 12:57:32 +0300

    Nothing much changed

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app>
     <div class="row py-4 mb-3">
-        <div class="col-sm-8">
+        <div class="col-lg-8 col-md-8 col-sm-12">
             <ul class="list-group">
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <span class="slate">Messages <br>
@@ -20,7 +20,7 @@
         </div>
         {{-- Empty space --}}
         <div class="col-sm-4">
-            
+
         </div>
     </div>
 </x-layouts.app>
